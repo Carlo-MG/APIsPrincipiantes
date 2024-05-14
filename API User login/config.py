95a26a91c441bb70dbf21dc8c0196056c8219815
Carlo-MG <145179622+Carlo-MG@users.noreply.github.com>
2024-05-14 13:26:23 -0600

    API login user

---
+++ API User login/config.py
@@ -0,0 +1,5 @@
+
+class Config():
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/login'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ README.md
@@ -0,0 +1,3 @@
+<section>
+    <h1 align='center'>APIsPrincipiantes</h1>
+</section>
